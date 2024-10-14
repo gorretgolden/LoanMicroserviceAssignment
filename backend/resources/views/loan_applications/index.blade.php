34d483778f2df420253870eaf4a2d3a09f3a3718
gorret golden <nabatanzigorret143@gmail.com>
2024-10-14 09:43:13 +0300

    removed add loans on admin for now

--- backend/resources/views/loan_applications/index.blade.php
+++ backend/resources/views/loan_applications/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 @section('title')
-    Loan Applications 
+    Loan Applications
 @endsection
 @section('content')
     <section class="section">
         <div class="section-header">
             <h1>Loan Applications</h1>
-            <div class="section-header-breadcrumb">
+            {{-- <div class="section-header-breadcrumb">
                 <a href="{{ route('loanApplications.create')}}" class="btn btn-primary form-btn">Loan Application <i class="fas fa-plus"></i></a>
-            </div>
+            </div> --}}
         </div>
     <div class="section-body">
        <div class="card">
@@ -17,7 +17,7 @@
             </div>
        </div>
    </div>
-    
+
     </section>
 @endsection
 
